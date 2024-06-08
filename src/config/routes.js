6635904452a319506038aad02637afac3b2254f5
Dimitar Dimitrov <dimitardimitrov0317@gmail.com>
2024-06-08 18:34:27 +0300

    above and 404 controllers

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,6 +1,12 @@
-const {Router} = require('express');
+const { Router } = require('express');
+const { notFound } = require('../controllers/404');
+const { about } = require('../controllers/about');
 
 
-const router=Router();
+const router = Router();
+
+router.get('/about', about);
 
 router.get('*', notFound);
+
+module.exports = { router };
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const handlebars = require('express-handlebars')
 const path = require('path');
 const { getAllMovies, getMovieById } = require('./services/movieService');
 const { configDatabase } = require('./config/database');
+const { router } = require('./config/routes');
 
 const app = express();
 const port = 5000;
@@ -17,31 +18,32 @@ async function start() {
     
     app.use(express.static('src/static'))
     
-    app.get('/', async (req, res) => {
-        const movies = await getAllMovies();
-        res.render('home', { movies });
-    })
-    app.get('/create/movie', (req, res) => {
-        res.render('create');
-    })
-    app.get('/create/cast', (req, res) => {
-        res.render('cast-create');
-    })
-    app.get('/search', (req, res) => {
-        res.render('search');
-    })
-    app.get('/about', (req, res) => {
-        res.render('about');
-    })
-    app.get('/details/:id', async (req, res) => {
-        const id = req.params.id;
-        const movie = await getMovieById(id);
-        movie.starRating = '&#x2605;'.repeat(movie.rating);
-        res.render('details', { movie });
-    })
-    app.get('*', (req, res) => {
-        res.render('404', { layout: false });
-    })
+    app.use(router);
+    // app.get('/', async (req, res) => {
+    //     const movies = await getAllMovies();
+    //     res.render('home', { movies });
+    // })
+    // app.get('/create/movie', (req, res) => {
+    //     res.render('create');
+    // })
+    // app.get('/create/cast', (req, res) => {
+    //     res.render('cast-create');
+    // })
+    // app.get('/search', (req, res) => {
+    //     res.render('search');
+    // })
+    // app.get('/about', (req, res) => {
+    //     res.render('about');
+    // })
+    // app.get('/details/:id', async (req, res) => {
+    //     const id = req.params.id;
+    //     const movie = await getMovieById(id);
+    //     movie.starRating = '&#x2605;'.repeat(movie.rating);
+    //     res.render('details', { movie });
+    // })
+    // app.get('*', (req, res) => {
+    //     res.render('404', { layout: false });
+    // })
     app.listen(port, () => console.log(`Server is listening on port ${port}...`));
 };
 start();
