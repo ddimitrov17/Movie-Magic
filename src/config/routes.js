5caa71cc122775124c855b914a4aea89b95aeff0
Dimitar Dimitrov <dimitardimitrov0317@gmail.com>
2024-06-10 14:37:58 +0300

    routes and controllers added

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,11 +1,19 @@
 const { Router } = require('express');
 const { notFound } = require('../controllers/404');
 const { about } = require('../controllers/about');
+const { home, details, search } = require('../controllers/catalog');
+const { createGet } = require('../controllers/createMovie');
+const { createGet: createCastGet } = require('../controllers/createCast');
 
 
 const router = Router();
 
+router.get('/', home);
 router.get('/about', about);
+router.get('/details/:id', details);
+router.get('/create/movie', createGet);
+router.get('/create/cast', createCastGet);
+router.get('/search', search);
 
 router.get('*', notFound);
 
